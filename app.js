03467478065d4be20369a710b4aace48928ab1aa
jonrothermel <31423582+jonrothermel@users.noreply.github.com>
2017-12-09 16:39:10 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const modal = function(url) {
+const modal = function(e) {
 	const body = $('body')
 	body.css('overflow', 'hidden')
 	body.css('position', 'fixed')
@@ -35,103 +35,46 @@ const modal = function(url) {
 	content.css({
 		height: '80%',
 		width: '80%',
-		margin: '20% auto',
+		margin: '10% auto',
 		backgroundColor: 'white',
 		boxSizing: 'border-box',
 		overflow: 'scroll'
+		
 	})
 
 	shim.append(close)
 	shim.append(content)
 	body.append(shim);
 
-	$.ajax({
-			url: 'https://wt-c5c9dbbf840b9daa284546a2e7dc37cb-0.run.webtask.io/get_locations',
-			
-			reload: true,
-			success: function(response) {
-				content.empty()
-				const div = $(response);
-				const data = JSON.parse(div.text())
-				console.log(data)
-				const neighborhood = data[0].fields.neighborhood
-				const name = (data[0].fields.name)
-				const address = (data[0].fields.address)
-				const description = data[0].fields.description
-				const pictureUrl = (data[0].fields.picture[0].thumbnails.large.url)
+	const currentNeighborhood = $(e.target).text()
+	$.get('https://wt-c5c9dbbf840b9daa284546a2e7dc37cb-0.run.webtask.io/get_locations')
+		.then(function(data) {
+			content.empty();
+			data.forEach(function(row, index) {
+				const neighborhood = data[index].fields.neighborhood
+				if (neighborhood[0] !== currentNeighborhood) {
+					return;
+				}
+				const name = (data[index].fields.name)
+				const address = (data[index].fields.address)
+				const description = data[index].fields.description
+				const pictureUrl = (data[index].fields.picture[0].thumbnails.large.url)
 				const table = `
-<div class="card mb-3">
-  <img class="card-img-top" src="${pictureUrl}" alt="image">
-  <div class="card-body">
-    <h4 class="card-title">${name}</h4>
-    <p class="card-text"><strong>Description:</strong> ${description}</p>
-    <p class="card-text"><strong>Address:</strong> ${address}</p>
-  </div>
-</div>
-<div class="card mb-3">
-  <img class="card-img-top" src="${pictureUrl}" alt="image">
-  <div class="card-body">
-    <h4 class="card-title">${name}</h4>
-    <p class="card-text"><strong>Description:</strong> ${description}</p>
-    <p class="card-text"><strong>Address:</strong> ${address}</p>
-  </div>
-</div>
-				`;
-				content.html(table)
-
-			}
-		});
+	<div class="card mb-3">
+	  <img class="card-img-top" src="${pictureUrl}" alt="image">
+	  <div class="card-body">
+	    <h4 class="card-title">${name}</h4>
+	    <p class="card-text"><strong>Description:</strong> ${description}</p>
+	    <p class="card-text"><strong>Address:</strong> ${address}</p>
+	  </div>
+	</div>
+					`;
+					content.append(table)
+			});
+			
+		})
 }
-$('.neighborhood-list-item ').click(function(){
-	modal();
-})
-// $('.neighborhood-list-item ').each(function(index, currentListItem) {
-// 	$(currentListItem).addClass('js-modal-downtown-manhattan-'+index)
-// 	new jBox('Modal', {
-// 	    width: 800,
-// 	    height: 400,
-// 	    attach: '.js-modal-downtown-manhattan-'+index,
-// 	    title: 'What is in Manhattan?',
-// 		ajax: {
-// 			url: 'https://wt-c5c9dbbf840b9daa284546a2e7dc37cb-0.run.webtask.io/get_locations',
-// 			data: {
-// 			  id: index,
-// 			},
-// 			reload: true,
-// 			success: function(response) {
-// 				$('.jBox-content').empty()
-// 				const div = $(response);
-// 				const data = JSON.parse(div.text())
-// 				console.log(data)
-// 				const neighborhood = data[0].fields.neighborhood
-// 				const name = (data[0].fields.name)
-// 				const address = (data[0].fields.address)
-// 				const description = data[0].fields.description
-// 				const pictureUrl = (data[0].fields.picture[0].thumbnails.large.url)
-// 				const table = `
-// <div class="card mb-3">
-//   <img class="card-img-top" src="${pictureUrl}" alt="image">
-//   <div class="card-body">
-//     <h4 class="card-title">${name}</h4>
-//     <p class="card-text"><strong>Description:</strong> ${description}</p>
-//     <p class="card-text"><strong>Address:</strong> ${address}</p>
-//   </div>
-// </div>
-// <div class="card mb-3">
-//   <img class="card-img-top" src="${pictureUrl}" alt="image">
-//   <div class="card-body">
-//     <h4 class="card-title">${name}</h4>
-//     <p class="card-text"><strong>Description:</strong> ${description}</p>
-//     <p class="card-text"><strong>Address:</strong> ${address}</p>
-//   </div>
-// </div>
-// 				`;
-// 				$('.jBox-content').html(table)
-
-// 			}
-// 		},
-// 	})
-// });
+$('.neighborhood-list-item ').click(modal)
 
 const onClick = function (e) {
 	// e.preventDefault();
